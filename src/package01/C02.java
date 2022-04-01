1ac4af5307832e29101bb6b0173401b5565d3ee1
hcrnCey <mc368958@gmail.com>
2022-04-01 17:04:07 +0300

    Buda ikinci commit

--- src/package01/C02.java
+++ src/package01/C02.java
@@ -1,8 +1,7 @@
 package package01;
 
-public class C01 {
+public class C02 {
     public static void main(String[] args) {
-
-        System.out.println("Hello word");
+        System.out.println("Merhaba Dunya");
     }
 }
